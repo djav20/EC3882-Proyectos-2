50d2bce23a68a799a097c84347a988d4dd663ad4
Gabriel <gabrielnoya95@gmail.com>
2018-02-21 16:11:37 -0400

    imagenes rotando

--- Juego/public/cell.js
+++ Juego/public/cell.js
@@ -1,14 +1,24 @@
 class Cell {
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-        this.hasMonster = false;
-    }
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.hasMonster = false;
+  }
 }
 
 function adjustCells(){
-    var wx = Math.floor(width / m);
-    var wy = Math.floor(height / n);
-    if(wx<wy) return wx;
-    else return wy;
+  var wx = Math.floor(width / columns);
+  var wy = Math.floor(height / rows);
+  if(wx<wy) return wx;
+  else return wy;
+}
+
+function initiateCells(){
+  for(var i = 0; i < rows; i++){
+    for(var j = 0; j < columns; j++){
+      var x = j * w;
+      var y = i * w;
+      cells.push(new Cell(x,y));
+    }
+  }
 }
